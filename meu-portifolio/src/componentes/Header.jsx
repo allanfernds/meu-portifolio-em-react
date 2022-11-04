89de796ad85e13251d8acd0c15856968d2f95c47
allanfernds <allanfernds@gmail.com>
2022-11-04 18:21:21 -0300

    create: nav link and logo span

--- meu-portifolio/src/componentes/Header.jsx
+++ meu-portifolio/src/componentes/Header.jsx
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <header>
+        <div>
+          <nav>
+            <span>Logo</span>
+            <ul>
+              <li>About</li>
+              <li>Skills</li>
+              <li>Projects</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
   }
 }
