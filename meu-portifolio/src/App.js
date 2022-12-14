08b4dbd2152881164e1075d61f2a38dbbe6ba9ab
allanfernds <allanfernds@gmail.com>
2022-12-13 21:34:59 -0300

    create skills section and instal font awesome

--- meu-portifolio/src/App.js
+++ meu-portifolio/src/App.js
@@ -1,12 +1,15 @@
-import About from "./SkillsSection.jsx/About";
-import Header from "./SkillsSection.jsx/Header";
-import Hero from './SkillsSection.jsx/Hero'
+import About from "./components/About";
+import Header from "./components/Header";
+import Hero from './components/Hero'
+import SkillsSection from './components/SkillsSection'
+
 function App() {
   return (
     <div className="bg-black">
       <Header />
       <Hero />
       <About />
+      <SkillsSection />
     </div>
   );
 }
