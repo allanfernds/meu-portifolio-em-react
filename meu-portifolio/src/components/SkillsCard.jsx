03438516dfefa736254fca569b93738f8d26d52b
allanfernds <allanfernds@gmail.com>
2022-12-14 04:21:14 -0300

    component and atyle: skills section and card

---
+++ meu-portifolio/src/components/SkillsCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class SkillsCard extends Component {
+  render() {
+    const { stack, icon } = this.props;
+    return (
+      <div className="inline-block p-8 text-center border">
+        <div className="p-1 flex items-center justify-center w-14 h-14 mx-auto mb-4 rounded-full bg-indigo-50">
+          <img src={icon} alt={stack} width="40" />
+        </div>
+        <p className="font-bold tracking-wide text-gray-800">{stack}</p>
+      </div>
+    );
+  }
+}
--- meu-portifolio/src/components/SkillsSection.jsx
+++ meu-portifolio/src/components/SkillsSection.jsx
@@ -1,116 +1,54 @@
 import React, { Component } from "react";
+import SkillsCard from "./SkillsCard";
+import tailwindIcon from "../assets/images/tailwindIcon.png";
 
 export default class SkillsSection extends Component {
   render() {
     return (
       <div className="mb-16">
-        <div className="bg-gray-100">
+        <div className="bg-white ">
           <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-              <div>
-                <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-                  Brand new
-                </p>
-              </div>
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-                <span className="relative inline-block">
-                  <svg
-                    viewBox="0 0 52 24"
-                    fill="currentColor"
-                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-gray-400 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-                  >
-                    <defs>
-                      <pattern
-                        id="dc223fcc-6d72-4ebc-b4ef-abe121034d6e"
-                        x="0"
-                        y="0"
-                        width=".135"
-                        height=".30"
-                      >
-                        <circle cx="1" cy="1" r=".7" />
-                      </pattern>
-                    </defs>
-                    <rect
-                      fill="url(#dc223fcc-6d72-4ebc-b4ef-abe121034d6e)"
-                      width="52"
-                      height="24"
-                    />
-                  </svg>
-                  <span className="relative">The</span>
-                </span>{" "}
-                quick, brown fox jumps over a lazy dog
-              </h2>
-              <p className="text-base text-gray-700 md:text-lg">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque rem aperiam, eaque ipsa quae.
-              </p>
-            </div>
-            <div className="flex items-center sm:justify-center">
-              <button
-                type="submit"
-                className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Get started
-              </button>
-              <a
-                href="/"
-                aria-label=""
-                className="inline-flex items-center font-semibold text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700"
-              >
-                Learn more
-              </a>
-            </div>
+            <h3 className="text-center pt-5 text-2xl font-bold text-black md:text-4xl">
+              Skills
+            </h3>
           </div>
         </div>
         <div className="relative px-4 sm:px-0">
-          <div className="absolute inset-0 bg-gray-100 h-1/2" />
+          <div className="absolute inset-0 bg-white h-1/2" />
           <div className="relative grid mx-auto overflow-hidden bg-white divide-y rounded shadow sm:divide-y-0 sm:divide-x sm:max-w-screen-sm sm:grid-cols-3 lg:max-w-screen-md">
-            <div className="inline-block p-8 text-center">
-              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-10 h-10 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="font-bold tracking-wide text-gray-800">
-                Make it better
-              </p>
-            </div>
-            <div className="inline-block p-8 text-center">
-              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50">
-                <svg
-                  className="w-10 h-10 text-deep-purple-accent-400"
-                  stroke="currentColor"
-                  viewBox="0 0 52 52"
-                >
-                  <polygon
-                    strokeWidth="3"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    fill="none"
-                    points="29 13 14 29 25 29 23 39 38 23 27 23"
-                  />
-                </svg>
-              </div>
-              <p className="font-bold tracking-wide text-gray-800">
-                Do it faster
-              </p>
-            </div>
-            <div className="inline-block p-8 text-center">
-              <div className="flex items-center justify-center w-12 h-12 mx-auto mb-4 rounded-full bg-indigo-50"></div>
-              <p className="font-bold tracking-wide text-gray-800">
-                Working harder
-              </p>
-            </div>
+            <SkillsCard
+              stack="JavaScript"
+              icon="https://img.icons8.com/ios/512/javascript.png"
+            />
+            <SkillsCard
+              stack="HTML"
+              icon="https://img.icons8.com/ios/512/html-5--v2.png"
+            />
+            <SkillsCard
+              stack="CSS"
+              icon="https://img.icons8.com/ios/512/css3.png"
+            />
+            <SkillsCard
+              stack="React"
+              icon="https://img.icons8.com/ios-filled/512/react-native.png"
+            />
+            <SkillsCard stack="TailWindCSS" icon={tailwindIcon} />
+            <SkillsCard
+              stack="Node"
+              icon="https://img.icons8.com/windows/512/node-js.png"
+            />
+            <SkillsCard
+              stack="Jest"
+              icon="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/512/external-jest-can-collect-code-coverage-information-from-entire-projects-logo-bold-tal-revivo.png"
+            />
+            <SkillsCard
+              stack="Python"
+              icon="https://img.icons8.com/ios/512/python.png"
+            />
+            <SkillsCard
+              stack="Git"
+              icon="https://img.icons8.com/windows/512/git.png"
+            />
           </div>
         </div>
       </div>
