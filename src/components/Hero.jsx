250e92a2bf4adfe1b4d65ef3a93a224072d02a44
Alan Fernandes <allanfernds@gmail.com>
2024-02-16 20:45:56 -0300

    update Hero.jsx, NavBar.jsx and src/style/index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import minhaLogo from '../assets/images/alanlogo.svg';
 
 function Index() {
   return (
-    <div className="py-20 overflow-y-hidden lg:py-48">
-      <div id="hero" className="pt-20 flex">
-        <div className="container flex flex-col items-center py-12 sm:py-24">
-          <div className="w-11/12 sm:w-2/3 flex justify-center items-center md:items-start flex-col mb-5 sm:mb-10 gap-5">
+    <div className="py-20 lg:py-48">
+      <div id="hero" className="pt-20  flex">
+        <div className="  flex items-center justify-center">
+          <div className=" w-11/12 sm:w-5/6 flex justify-center  items-center md:items-start flex-col mb-5 sm:mb-10 gap-5">
             <h1 className=" text-center sm:text-left text-3xl max-w-[600px] text-white leading-8 lg:text-4xl md:leading-8 font-archivo">
               Ajudando empresas a construirem{' '}
               <span className="animate-bounce font-medium text-sky-400">
@@ -14,8 +14,8 @@ function Index() {
               modernas e de alta qualidade
             </h1>
             <p className="max-w-[600px] text-center text-gray-300 md:text-left text-2xl font-archivo font-thin">
-              Automatizo processos, gerencio E-commerces, desenvolvo sistemas e
-              uns sites bonitos
+              Automatizo processos, gerencio <br /> E-commerces, desenvolvo
+              sistemas <br /> e uns sites bonitos
             </p>
             <a
               className="bg-gradient-to-l from-sky-600 to-blue-500 w-[150px] block mt-2 text-center py-2 rounded-sm text-white hover:scale-105 ease-out transition-all text-lg px-2"
@@ -27,8 +27,8 @@ function Index() {
             </a>
           </div>
         </div>
-        <figure className="hidden w-[900px] md:block">
-          <img src={minhaLogo} alt="logo alan fernandes" loading="lazy" draggable="false" />
+        <figure className="hidden md:block">
+          <img src={minhaLogo} alt="logo alan fernandes" draggable="false" />
         </figure>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import minhaLogo02 from '../assets/images/alanlogo03.svg';
 import { List, X } from '@phosphor-icons/react';
 
 export default function NavBar() {
-  const [navbar, setNavbar] = useState(true);
+  const [navbar, setNavbar] = useState(false);
   const [showNavBar, hideNavBar] = useState(true);
   const [lastScrollTop, setLastScrollTop] = useState(0);
 
@@ -35,19 +35,28 @@ export default function NavBar() {
       <div className="justify-between px-4 lg:px-32 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8  bg-transparent ">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block ">
-            <a draggable="false" href="#hero" className="flex justify-center items-stretch lg:gap-4 gap-2">
-              <img
+            <a
               draggable="false"
+              href="#hero"
+              className="flex justify-center items-stretch lg:gap-4 gap-2"
+            >
+              <img
+                draggable="false"
                 className="w-7 lg:w-10"
                 src={minhaLogo02}
                 alt="logo alan fernandes"
               />
               <h2 className="font-normal text-white text-base leading-4 lg:text-xl lg:leading-6  font-archivo ">
-                Alan<br />Fernandes
+                Alan
+                <br />
+                Fernandes
               </h2>
             </a>
             <div className="md:hidden transition-all">
-              <button alt="abrir e fechar menu" onClick={() => setNavbar(!navbar)}>
+              <button
+                alt="abrir e fechar menu"
+                onClick={() => setNavbar(!navbar)}
+              >
                 {navbar ? (
                   <X size={32} color="#38bdf8" weight="bold" />
                 ) : (
@@ -64,21 +73,32 @@ export default function NavBar() {
             }`}
           >
             <ul className="items-end justify-end font-bold space-y-8 md:flex md:space-x-6 text-center md:space-y-0 bg-transparent font-comfortaa">
-
               <li className="relative text-gray-100 group">
-                <a onClick={() => setNavbar(false)} href="#projects" className="no-underline">
+                <a
+                  onClick={() => setNavbar(false)}
+                  href="#projects"
+                  className="no-underline"
+                >
                   Projetos
                   <span className="invisible md:visible absolute w-full h-1 bg-gradient-to-r from-transparent via-sky-400 to-sky-500 bottom-0 left-0 origin-left transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100 rounded-2xl"></span>
                 </a>
               </li>
               <li className="relative text-gray-100 group">
-                <a onClick={() => setNavbar(false)}  href="#services" className="no-underline">
+                <a
+                  onClick={() => setNavbar(false)}
+                  href="#services"
+                  className="no-underline"
+                >
                   Serviços
                   <span className="invisible md:visible absolute w-full h-1 bg-gradient-to-r from-transparent via-sky-400 to-sky-500 bottom-0 left-0 origin-left transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100 rounded-2xl"></span>
                 </a>
               </li>
               <li className="relative text-gray-100 group">
-                <a onClick={() => setNavbar(false)}  href="#contact" className="no-underline">
+                <a
+                  onClick={() => setNavbar(false)}
+                  href="#contact"
+                  className="no-underline"
+                >
                   Contato
                   <span className="invisible md:visible absolute w-full h-1 bg-gradient-to-r from-transparent via-sky-400 to-sky-500 bottom-0 left-0 origin-left transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100 rounded-2xl"></span>
                 </a>
